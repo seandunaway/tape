d711f38f7fbee1e21d154eebc118932b6286df01
seandunaway <git@seandunaway.com>
2023-12-30 14:30:53 -0800

    sc2html

---
+++ sc2html.mjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import {readFileSync, writeFileSync} from 'node:fs'
+
+let input_file = process.argv[2]
+let input_content = readFileSync(input_file, {encoding: 'utf8'})
+let input_lines = input_content.split('\n')
+
+let output_lines = []
+for (let line of input_lines) {
+	if (!line) continue
+	if (line.startsWith('Date')) continue
+
+	let fields = line.split(', ')
+	let date = fields[0]
+	let time = fields[1]
+	let last = fields[5]
+
+	output_lines.push(`${date} ${time} ${last}`)
+}
+
+let output_content = `\
+<!DOCTYPE html>
+<style>body {font-size: 8px;}
+@media print {#content {column-count: 6;}}
+</style>
+<pre id="content">
+${output_lines.join('\n')}
+</pre>
+`
+
+let output_file = `${input_file}.html`
+writeFileSync(output_file, output_content)
